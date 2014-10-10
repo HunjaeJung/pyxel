50da78e020f72940ed2770fe29e523201b6e1d4e
Hunjae <hunjaege@gmail.com>
2014-10-10 23:00:11 +0900

    left-right change

--- image.py
+++ image.py
@@ -24,7 +24,7 @@ pixels = im.load()
 # write pixel info as rgb565 format to header file
 for x in range(width):
     for y in range(height):
-        cpixel = pixels[x, y]
+        cpixel = pixels[width-1-x, y]
         hexrgb=hex((((cpixel[0]&0xf8)>>3)<<11)|(((cpixel[1]&0xfc)>>2)<<5)|(((cpixel[2]&0xf8)>>3)))
         file.write(hexrgb)
         if(x*height+y!=width*height-1):
