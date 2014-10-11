9f5a76ea0b13deca2a79958c8397000036f070d3
Hunjae <hunjaege@gmail.com>
2014-10-11 20:19:19 +0900

    change to true/false mode

--- image.py
+++ image.py
@@ -2,9 +2,11 @@ from PIL import Image  # need to install PIL
 import sys
 
 # User input image file name
-# ex) python image.py pic.jpg
-if len(sys.argv)!=2:
-    print("Image file name needed")
+# ex) python image.py true.jpg 1   : it will make trueimg.h
+# ex) python image.py false.jpg 2  : it will make falseimg.h
+
+if len(sys.argv)!=3:
+    print("Image filename and true/false option(1/2) needed")
     sys.exit()
 
 i = Image.open(sys.argv[1])
@@ -15,7 +17,11 @@ height=240
 
 # .h file would be created (the header file only include rgb565 format array)
 filename=sys.argv[1].split('.',1)[0];
-file = open(filename+".h","w")
+if(int(sys.argv[2])==1):
+	file = open("trueimg.h","w")
+else:
+	file = open("falseimg.h","w")
+
 file.write("const u16 "+filename+"["+str(width*height)+"] = {");
 
 im=i.resize((width,height), Image.NEAREST) # https://www.daniweb.com/software-development/python/code/216637/resize-an-image-python
@@ -32,6 +38,9 @@ for x in range(width):
 
 # close
 file.write("};")
-print(filename+".h is successfully created!")
+if(int(sys.argv[2])==1):
+	print("trueimg.h is successfully created!")
+else:
+	print("falseimg.h is successfully created!")
 file.close()
 
